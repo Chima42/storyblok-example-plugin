c64ac4a78d552517813b44a2546ee46b036fa082
Chima Nwosu <chimannwosu@gmail.com>
2023-12-12 20:26:19 +0000

    added uea plugin

---
+++ uea-plugin/src/App.tsx
@@ -0,0 +1,11 @@
+// import FieldPlugin from './components/FieldPlugin'
+// import FieldPluginExample from './components/FieldPluginExample'
+import { FunctionComponent } from 'react'
+// import MyPlugin from './components/MyPlugin/MyPlugin'
+import WAVECategories from './components/uea/uea'
+
+const App: FunctionComponent = () => {
+  return <WAVECategories />
+}
+
+export default App
